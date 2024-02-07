e6eeda259b7b766891a49b9c35d5a35316f5fcbb
hellomaxime <86252821+hellomaxime@users.noreply.github.com>
2024-02-07 23:21:09 +0100

    handle multiple concurrent clients

--- redis_server.py
+++ redis_server.py
@@ -1,13 +1,11 @@
 from utils import deserialize, process, serialize
 import socket
+from _thread import start_new_thread
 
 HOST = "127.0.0.1"
 PORT = 6379
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
+def new_client(conn, addr):
     with conn:
         print(f"Connection from {addr}")
         while True:
@@ -16,3 +14,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 break
             response = serialize(process(deserialize(data.decode())))
             conn.sendall(response.encode())
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    while True:
+        conn, addr = s.accept()
+        start_new_thread(new_client, (conn, addr))
